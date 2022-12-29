dcac51509ec48e71154d832d4a9f9a5dae72c2ba
Anton <whafu@kth.se>
2022-12-29 12:22:28 +0100

    Compute metrics and save model

--- Anomaly Detection with Autoencoder/dataloader.py
+++ Anomaly Detection with Autoencoder/dataloader.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch    
 
-NOISE_BOUND = 0.3
+NOISE_BOUND = 0.1
 IMG_DIM = 28
 MNIST_PATH = r"/home/anton/KTH/year5/5LSH0/mnist"
 
@@ -35,7 +35,7 @@ class DataLoader():
 
         return noisy_image
 
-    def prepareMNIST(self, mnist_path, num_train_data, num_normal_data, num_anomaly_data, corrupt_train=None):
+    def prepareMNIST(self, mnist_path, num_train_data, num_normal_data, num_anomaly_data, corrupt_train=None, noise_bound=0.3):
         mndata = MNIST(mnist_path)
         train_images, _ = mndata.load_training()
 
@@ -46,12 +46,12 @@ class DataLoader():
         normal_train = np.array(normal_train)
         # Corrupt training data
         if corrupt_train!=None:
-            corrupt_data = np.array([self.addNoise((image), noise_bound=NOISE_BOUND) for image in normal_train])
+            corrupt_data = np.array([self.addNoise((image), noise_bound=noise_bound) for image in normal_train])
             num_corrupt_data = int(len(normal_train) * corrupt_train)
             normal_train = np.concatenate([corrupt_data[:num_corrupt_data], normal_train[num_corrupt_data:]], axis=0)
         
         normal_data = np.array(normal_data)
-        anomaly_data = np.array([self.addNoise((image), noise_bound=NOISE_BOUND) for image in anomaly_data])
+        anomaly_data = np.array([self.addNoise((image), noise_bound=noise_bound) for image in anomaly_data])
 
         # Reshape and noramlize data
         normal_train = normal_train.reshape(num_train_data, 1, IMG_DIM, IMG_DIM) / 255
@@ -76,16 +76,19 @@ class DataLoader():
         train_images, train_labels = self.normal_train
         normal_images, normal_labels = self.normal_data
         anomaly_images, anomaly_labels = self.anomaly_data
+        val_images, val_labels = torch.cat([normal_images, anomaly_images], axis=0), torch.cat([normal_labels, anomaly_labels], axis=0) 
 
         train = torch.utils.data.TensorDataset(train_images, train_labels)
+        val = torch.utils.data.TensorDataset(val_images, val_labels)
         normal = torch.utils.data.TensorDataset(normal_images, normal_labels)
         anomaly = torch.utils.data.TensorDataset(anomaly_images, anomaly_labels)
 
         train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size, shuffle=True)
         normal_loader = torch.utils.data.DataLoader(normal, batch_size=batch_size, shuffle=True)
         anomaly_loader = torch.utils.data.DataLoader(anomaly, batch_size=batch_size, shuffle=True)
         
-        return train_loader, normal_loader, anomaly_loader
+        return train_loader, val_loader, normal_loader, anomaly_loader
 
 if __name__ == '__main__':
     dl = DataLoader()
@@ -96,19 +99,20 @@ if __name__ == '__main__':
         num_anomaly_data=10,
         corrupt_train=0.5
     )
-    train_loader, normal_loader, anomaly_loader = dl.getDataLoaderMNIST(10)
+    train_loader, val_loader, normal_loader, anomaly_loader = dl.getDataLoaderMNIST(10)
     train_images = dl.normal_train[0]
     normal_images = dl.normal_data[0]
     anomaly_images = dl.anomaly_data[0]
 
-    for i, img in enumerate(train_images):
-        # print(i)
+    for i, (images, label) in enumerate(val_loader):
+        for image in images:
         # img_idx = np.random.randint(len(normal_images))
-        plt.imshow(img.reshape(IMG_DIM, IMG_DIM))
-        plt.show()
+            plt.imshow(image.reshape(IMG_DIM, IMG_DIM))
+            plt.show()
 
         # img_idx = np.random.randint(len(anomaly_images))
         # plt.imshow(anomaly_images[img_idx].reshape(IMG_DIM, IMG_DIM))
+    
         # plt.show()
 
 
